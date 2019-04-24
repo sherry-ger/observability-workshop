891a63710e551c6d8072c869c39e561958d06b65
Hemant Malik <hemant@elastic.co>
2019-04-24 03:37:38 +0000

    clean up in the script

--- prereqs-setup.ps1
+++ prereqs-setup.ps1
@@ -1,16 +1,22 @@
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-$workshopPath = "C:\Users\Administrator\Desktop\observability-with-elastic"
-$prereq_1 = 'https://notepad-plus-plus.org/repository/7.x/7.6.6/npp.7.6.6.Installer.x64.exe'
+$workshopPath = "C:\Users\Administrator\Desktop\workshop-content"
+$downloadPath = "C:\Users\Administrator\Downloads\"
+# Beats used in the workshop
 $filebeat_link = 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.0.0-windows-x86_64.zip'
 $metricbeat_link = 'https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.0.0-windows-x86_64.zip'
 $heartbeat_link = 'https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-7.0.0-windows-x86_64.zip'
-function Install-Prereqs ()
+
+
+function Setup-Prereqs ()
 {    
+	# Notepad++ for users to work with files	
+	$prereq_1 = 'https://notepad-plus-plus.org/repository/7.x/7.6.6/npp.7.6.6.Installer.x64.exe'
+	
     If (!(Test-Path -Path $downloadPath -PathType Container)) {New-Item -Path $downloadPath -ItemType Directory | Out-Null}
     
     $packages = @(
-    @{title='Notepad++ 7.6.6';url=$prereq_1;Arguments=' /Q /S';Destination=$downloadPath},
-    @{title='Git 2.21.0';url=$prereq_2;Arguments=' /VERYSILENT /SUPPRESSMSGBOXES';Destination=$downloadPath}
+    @{title='Notepad++ 7.6.6';url=$prereq_1;Arguments=' /Q /S';Destination=$downloadPath}
+	#add other prereq details here
     )
     
     foreach ($package in $packages) {
@@ -32,27 +38,26 @@ function Install-Prereqs ()
         $destinationPath = $package.Destination + "\" + $fileName
         $Arguments = $package.Arguments
         Write-Output "Installing $packageName"
-    
-    
-    Invoke-Expression -Command "$destinationPath $Arguments"
+		Invoke-Expression -Command "$destinationPath $Arguments"
     }
 }
+# Download and Unzip Beats
 function Download-Beats(){
-    If (!(Test-Path -Path $downloadPath -PathType Container)) {New-Item -Path $downloadPath -ItemType Directory | Out-Null}
+    If (!(Test-Path -Path $workshopPath -PathType Container)) {New-Item -Path $workshopPath -ItemType Directory | Out-Null}
 
     $beats = @(
-    @{title='Filebeat';url=$filebeat_link;Arguments=' /Q /S';Destination=$downloadPath},
-    @{title='Metricbeat';url=$metricbeat_link;Arguments=' /Q /S';Destination=$downloadPath},
-    @{title='Heartbeat';url=$heartbeat_link;Arguments=' /Q /S';Destination=$downloadPath}
+    @{title='Filebeat';url=$filebeat_link;Arguments=''},
+    @{title='Metricbeat';url=$metricbeat_link;Arguments=''},
+    @{title='Heartbeat';url=$heartbeat_link;Arguments=''}
     )
     
     foreach ($beat in $beats) {
             $beatName = $beat.title
             $fileName = Split-Path $beat.url -Leaf
-            $destinationPath = $beat.Destination + "\" + $fileName
+            $destinationPath = $workshopPath + "\" + $fileName
+			Write-Output "Beats Download Location =  $destinationPath"
     
             If (!(Test-Path -Path $destinationPath -PathType Leaf)) {
-            
                 Write-Host "Downloading $beatName"
                 $webClient = New-Object System.Net.WebClient
                 $webClient.DownloadFile($beat.url,$destinationPath)
@@ -62,11 +67,11 @@ function Download-Beats(){
     foreach ($beat in $beats) {
         $beatName = $beat.title
         $fileName = Split-Path $beat.url -Leaf
-        $destinationPath = $beat.Destination + "\" + $fileName
-        $Arguments = $package.Arguments
+        $zipFile = $workshopPath + "\" + $fileName
         Write-Output "Unzipping $beatName"
-    
-        [System.IO.Compression.ZipFile]::ExtractToDirectory(($downloadPath+'\'+$fileName), $downloadPath)
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($zipFile, $workshopPath)
 
+	}
 }
-}
+Setup-Prereqs
+Download-Beats
